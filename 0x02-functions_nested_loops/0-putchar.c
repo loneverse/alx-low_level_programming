e50b74c7beef6cb1a69fcc31a8854a2cf006a72d
loneverse <jobungash@gmail.com>
2023-02-21 01:48:18 -0800

    program that prints _putchar, followed by a new line.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+int main(void)
+{
+_putchar(95); /*95 is asci for _*/
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+ * return (0);
+   }
