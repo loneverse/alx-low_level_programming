92f2dc917f4cf7dcdf2845775b905478a80a8e08
loneverse <jobungash@gmail.com>
2023-04-05 13:07:24 -0700

    hot fix

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -4,7 +4,7 @@
  * sum_listint - finds the sum of all the data(n) of a listint_t linked list
  * @head: pointer to the head node in the list
  *
- * Return: sum of all the data (n) of a listint_t linked list
+ * Return: find sum of all the data (n) of a listint_t linked list
  */
 int sum_listint(listint_t *head)
 {
