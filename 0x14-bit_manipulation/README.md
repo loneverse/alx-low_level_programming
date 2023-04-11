a960b97bbac37edbe4a6f1983c6756d7c713b147
loneverse <jobungash@gmail.com>
2023-04-11 05:20:37 -0700

    Initial commit

---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,3 @@
+# 0x14. C - Bit manipulation
+## In this Project:
+- Write functions to manipulate bits and use bitwise operators
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,7 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+int _putchar(char);
+unsigned int binary_to_uint(const char *b);
+
+#endif
