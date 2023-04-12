c859f4ebf2e9aa4217e38d90ecf10ca6d5fb8222
loneverse <jobungash@gmail.com>
2023-04-12 05:54:30 -0700

    function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,37 +14,26 @@
 
 int create_file(const char *filename, char *text_content)
 {
-    int file_descriptor, bytes_written;
-    int text_length = 0;
+	int fd, bytes_written;
+	int length = 0;
 
-    // If filename is NULL, return -1.
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    // If text_content is not NULL, get its length.
-    if (text_content != NULL)
-    {
-        for (text_length = 0; text_content[text_length]; text_length++)
-            ;
-    }
+	if (text_content != NULL)
+	{
+		for (length = 0; text_content[length];)
+			length++;
+	}
 
-    // Open the file with the specified filename.
-    // If the file already exists, truncate it.
-    // Set the file permissions to rw-------.
-    file_descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-    if (file_descriptor == -1)
-        return (-1);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	bytes_written = write(fd, text_content, length);
 
-    // Write the text_content to the file.
-    bytes_written = write(file_descriptor, text_content, text_length);
-    if (bytes_written == -1)
-    {
-        close(file_descriptor);
-        return (-1);
-    }
+	if (fd == -1 || bytes_written == -1)
+		return (-1);
 
-    // Close the file descriptor and return 1 to indicate success.
-    close(file_descriptor);
-    return (1);
+	close(fd);
+
+	return (1);
 }
 
